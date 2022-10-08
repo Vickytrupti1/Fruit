4cb69d7975e32c1404f8c1df840f3a1dd3ac99e8
unknown <vickysahoo702@gmail.com>
2022-10-08 12:59:32 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToSceool:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn"
+    });
+})
